97dd8369387217b9cf1bd5c26b0eec88fbc6f70e
Samuel Gouvêa <149209997+SamGouvea@users.noreply.github.com>
2023-10-27 22:18:09 -0300

    Interface e backEnd finalizados
    foi incrementado a interface e o calculo do IMC

--- CalculadoraIMC/Form1.Designer.cs
+++ CalculadoraIMC/Form1.Designer.cs
@@ -28,13 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnPeso = new System.Windows.Forms.Button();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(56, 38);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 0;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(56, 68);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // btnPeso
+            // 
+            this.btnPeso.Location = new System.Drawing.Point(21, 105);
+            this.btnPeso.Name = "btnPeso";
+            this.btnPeso.Size = new System.Drawing.Size(135, 26);
+            this.btnPeso.TabIndex = 2;
+            this.btnPeso.Text = "CALCULAR";
+            this.btnPeso.UseVisualStyleBackColor = true;
+            this.btnPeso.Click += new System.EventHandler(this.btnPeso_Click);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(12, 38);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 3;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(12, 68);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Altura";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.AllowDrop = true;
+            this.txtResultado.Location = new System.Drawing.Point(44, 147);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 150);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(26, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "IMC";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(17, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(139, 20);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "Calcule seu IMC";
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Location = new System.Drawing.Point(10, 174);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(0, 13);
+            this.lblResposta.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(177, 197);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.btnPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Cálculo do IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnPeso;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResposta;
     }
 }
 
--- CalculadoraIMC/Form1.cs
+++ CalculadoraIMC/Form1.cs
@@ -16,5 +16,58 @@ namespace CalculadoraIMC
         {
             InitializeComponent();
         }
+
+        private void btnPeso_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double pesot = double.Parse(txtPeso.Text);
+                double alturat = double.Parse(txtAltura.Text);
+
+                double result = (alturat * alturat) / pesot;
+
+
+                txtResultado.Text = result.ToString();
+
+                if (result < 18.5)
+                {
+                    lblResposta.Text = "Abaixo do peso!";
+                    lblResposta.ForeColor = Color.Red;
+                } 
+                else if (result < 24.9)
+                {
+                    lblResposta.Text = "Peso ideal (parabéns!)";
+                    lblResposta.ForeColor = Color.BlueViolet;
+                }
+                else if (result < 29.9)
+                {
+                    lblResposta.Text = "Levemente acima do peso!";
+                    lblResposta.ForeColor = Color.Yellow;
+                }
+                else if (result < 34.9)
+                {
+                    lblResposta.Text = "Obesidade grau I!";
+                    lblResposta.ForeColor = Color.DarkRed;
+                }
+                else if (result < 39.9)
+                {
+                    lblResposta.Text = "Obesidade grau II (severa)";
+                    lblResposta.ForeColor = Color.OrangeRed;
+                }
+                else if (result > 40)
+                {
+                    lblResposta.Text = "Obesidade III (mórbida)";
+                    lblResposta.ForeColor = Color.IndianRed;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Dados inválidos informados");
+                txtResultado.Clear();
+                txtPeso.Clear();
+                txtAltura.Clear();
+            }
+
+        }
     }
 }
